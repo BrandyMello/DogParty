ef8fc0a49f07c00a68fcd9369219deffab6605e6
Brandy Mello <brandyamello@gmail.com>
2019-03-18 10:04:00 -0600

    Add semicolons in js i am a monster

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
- var text = document.querySelector(".top span")
- var guess = document.querySelector("#guess")
- var button = document.querySelector("#nameButton")
+ var text = document.querySelector(".top span");
+ var guess = document.querySelector("#guess");
+ var button = document.querySelector("#nameButton");
 
  button.addEventListener("click", function(e) {
-  e.preventDefault()
-  text.innerText = guess.value
-  guess.value = ""
+  e.preventDefault();
+  text.innerText = guess.value;
+  guess.value = "";
  })
